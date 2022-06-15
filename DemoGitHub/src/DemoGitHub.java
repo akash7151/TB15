fd0f2ae2937d735b0650c768d0f92b456eafd425
akash7151 <107456835+akash7151@users.noreply.github.com>
2022-06-15 13:47:05 +0530

    by git

--- DemoGitHub/src/DemoGitHub.java
+++ DemoGitHub/src/DemoGitHub.java
@@ -4,11 +4,10 @@ public class DemoGitHub {
 	public void m1() {
 		System.out.println("m1");
 	}
-	public void m2() {
+	public void m4() {
 		System.out.println("M2");
 	}
-	public void m3() {
-		System.out.println("M3");
+	
 
 	}
 public static void main(String[] args) {
