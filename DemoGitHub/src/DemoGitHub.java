d7d98e358108a18160f0f1b461861b990482f424
akash7151 <107456835+akash7151@users.noreply.github.com>
2022-06-15 13:57:33 +0530

    By Branch

--- DemoGitHub/src/DemoGitHub.java
+++ DemoGitHub/src/DemoGitHub.java
@@ -4,6 +4,11 @@ public class DemoGitHub {
 	public void m1() {
 		System.out.println("m1");
 	}
+	public void m2(){
+		System.out.println("m2");
+	}
+	public void m3(){
+		System.out.println("m3");
 	public void m4() {
 		System.out.println("M2");
 	}
