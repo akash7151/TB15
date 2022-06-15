dc820914bc3708f2c4e823f95f7265741cc9e8ef
akash7151 <107456835+akash7151@users.noreply.github.com>
2022-06-15 13:43:14 +0530

    Update DemoGitHub.java

--- DemoGitHub/src/DemoGitHub.java
+++ DemoGitHub/src/DemoGitHub.java
@@ -4,6 +4,13 @@ public class DemoGitHub {
 	public void m1() {
 		System.out.println("m1");
 	}
+	public void m2() {
+		System.out.println("M2");
+	}
+	public void m3() {
+		System.out.println("M3");
+
+	}
 public static void main(String[] args) {
 	System.out.println("Git hub conceot");
 }
